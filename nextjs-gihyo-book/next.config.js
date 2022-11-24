f3051e38760f4088e5168bf895f3872542fd59ec
satoutai <118322639+satoutai@users.noreply.github.com>
2022-11-24 11:30:58 +0900

    3 files modified

--- nextjs-gihyo-book/next.config.js
+++ nextjs-gihyo-book/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler: {
+    //styledComponentsの有効化
+    styledComponents: true,
+  },
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
