c026ab2ba3a97a3499f1859b0829778070732d03
satoutai <118322639+satoutai@users.noreply.github.com>
2022-11-24 13:22:18 +0900

    2 files modified

--- nextjs-gihyo-book/src/pages/_app.tsx
+++ nextjs-gihyo-book/src/pages/_app.tsx
@@ -1,6 +1,47 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { createGlobalStyle } from 'styled-components';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+//グローバルのスタイル
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding:0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helverica Neue,sans-serif;
+  }
+  *{
+    box-sizing: border-box;
+  }
+  a{
+    cursor: pointer;
+    text-decoration: none;
+    transition: .25s;
+    color: #000;
+  }
+  ol,il{
+    list-style: none;
+  }
+`;
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <meta key="charset" name="charset" content="utf-8" />
+        <meta
+          key="viewport"
+          name="viewport"
+          content="width=device-width,initial-scale=1,shrink-to-fit=no,maximun-scale=5"
+        />
+        <meta property="og:locate" content="ja_JP" />
+        <meta property="og:type" content="website" />
+      </Head>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
+
